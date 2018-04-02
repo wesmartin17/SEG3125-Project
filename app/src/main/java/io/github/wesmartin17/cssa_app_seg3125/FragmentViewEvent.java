cd3f393efa6ad276449b87adc84a9901a219c887
wesmartin17 <wes.martin17@gmail.com>
2018-04-02 18:06:05 -0400

    someone else finish this page

--- app/src/main/java/io/github/wesmartin17/cssa_app_seg3125/FragmentViewEvent.java
+++ app/src/main/java/io/github/wesmartin17/cssa_app_seg3125/FragmentViewEvent.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +24,18 @@ public class FragmentViewEvent extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_view_event, container, false);
+        final TextView descriptionText = (TextView) view.findViewById(R.id.detail_description_content);
+        final TextView showAll = (TextView) view.findViewById(R.id.detail_read_all);
+        showAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAll.setVisibility(View.INVISIBLE);
+
+                descriptionText.setMaxLines(Integer.MAX_VALUE);
+            }
+        });
+        return view;
     }
 
 }
