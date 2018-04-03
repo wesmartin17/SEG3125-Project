e6b437b911295f47255ffc0785aee4828f1794c5
wesmartin17 <wes.martin17@gmail.com>
2018-04-03 11:37:10 -0400

    event page better

--- app/src/main/java/io/github/wesmartin17/cssa_app_seg3125/FragmentViewEvent.java
+++ app/src/main/java/io/github/wesmartin17/cssa_app_seg3125/FragmentViewEvent.java
@@ -1,12 +1,24 @@
 package io.github.wesmartin17.cssa_app_seg3125;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -14,6 +26,8 @@ import android.widget.TextView;
  */
 public class FragmentViewEvent extends Fragment {
 
+    Toolbar toolbar;
+    Button fbButton;
 
     public FragmentViewEvent() {
         // Required empty public constructor
@@ -25,6 +39,39 @@ public class FragmentViewEvent extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_view_event, container, false);
+        fbButton = (Button)view.findViewById(R.id.fbButton);
+        toolbar = (Toolbar)view.findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu_main);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getOrder()) {
+                    //favourite pressed
+                    case 0:
+                        item.setIcon(R.drawable.ic_star);
+                        Toast.makeText(getContext(),"Following event.  You will receive notifications about any changes to the event",Toast.LENGTH_LONG).show();
+                        return true;
+                    case 1:
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        intent.setType("plain/text");
+                        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"some@email.address"});
+                        intent.putExtra(Intent.EXTRA_SUBJECT, "Question about UPCOMINGEVENT");
+                        intent.putExtra(Intent.EXTRA_TEXT, "");
+                        startActivity(Intent.createChooser(intent, ""));
+                        return true;
+                }
+                return false;
+            }
+        });
+
         final TextView descriptionText = (TextView) view.findViewById(R.id.detail_description_content);
         final TextView showAll = (TextView) view.findViewById(R.id.detail_read_all);
         showAll.setOnClickListener(new View.OnClickListener() {
@@ -35,7 +82,49 @@ public class FragmentViewEvent extends Fragment {
                 descriptionText.setMaxLines(Integer.MAX_VALUE);
             }
         });
+
+        fbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newFacebookIntent(getContext().getPackageManager(),"https://www.facebook.com/CSSAAEI");
+            }
+        });
+
         return view;
+    }
+
+    /**
+     * <p>Intent to open the official Facebook app. If the Facebook app is not installed then the
+     * default web browser will be used.</p>
+     *
+     * <p>Example usage:</p>
+     *
+     * {@code newFacebookIntent(ctx.getPackageManager(), "https://www.facebook.com/JRummyApps");}
+     *
+     * @param pm
+     *     The {@link PackageManager}. You can find this class through {@link
+     *     Context#getPackageManager()}.
+     * @param url
+     *     The full URL to the Facebook page or profile.
+     * @return An intent that will open the Facebook page/profile.
+     */
+    public  Intent newFacebookIntent(PackageManager pm, String url) {
+        Uri uri = Uri.parse(url);
+        try {
+            ApplicationInfo applicationInfo = pm.getApplicationInfo("com.facebook.katana", 0);
+            if (applicationInfo.enabled) {
+                // http://stackoverflow.com/a/24547437/1048340
+                uri = Uri.parse("fb://facewebmodal/f?href=" + url);
+            }
+            else{
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+                browserIntent.setData(Uri.parse(url));
+                Intent chooserIntent = Intent.createChooser(browserIntent, "dialog title");
+                startActivity(chooserIntent);
+            }
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return new Intent(Intent.ACTION_VIEW, uri);
     }
 
 }
