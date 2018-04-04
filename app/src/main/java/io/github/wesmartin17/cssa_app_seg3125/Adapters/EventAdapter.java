ab9dc66893f1499ba550d14367c57651d86abe48
Mac Hayter <hayterm96@gmail.com>
2018-04-04 14:57:51 -0400

    Content and small layout changes

--- app/src/main/java/io/github/wesmartin17/cssa_app_seg3125/Adapters/EventAdapter.java
+++ app/src/main/java/io/github/wesmartin17/cssa_app_seg3125/Adapters/EventAdapter.java
@@ -37,22 +37,22 @@ public class EventAdapter extends RecyclerView.Adapter<EventViewHolder> {
 
         switch(position){
             case 0:
-                holder.setTitleText("Billiards Night @ Mclarens");
-                holder.setDescriptionText("Come out and shoot some billiards with some friends and muck some food");
+                holder.setTitleText("Research at EECS");
+                holder.setDescriptionText("Come see professors present their research!");
                 holder.setDateText("January 2, 7:00PM");
-                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.billiards));
+                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.research));
                 break;
             case 1:
-                holder.setTitleText("Lan Party");
-                holder.setDescriptionText("Come out and play games with some friends and muck some food");
+                holder.setTitleText("Friends Trivia at Mais");
+                holder.setDescriptionText("Come out and play trivia.");
                 holder.setDateText("January 20, 7:00PM");
-                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.billiards));
+                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.trivia));
                 break;
             case 2:
-                holder.setTitleText("Billiards Night @ Mclarens");
-                holder.setDescriptionText("Come out and shoot some billiards with some friends and muck some food");
+                holder.setTitleText("Loft Night");
+                holder.setDescriptionText("Come play games at the loft, free food included.");
                 holder.setDateText("January 22, 7:00PM");
-                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.billiards));
+                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.loft));
                 break;
             case 3:
                 holder.setTitleText("Lightning talks");
@@ -61,16 +61,16 @@ public class EventAdapter extends RecyclerView.Adapter<EventViewHolder> {
                 holder.setImgDrawable(context.getResources().getDrawable(R.drawable.lightning));
                 break;
             case 4:
-                holder.setTitleText("Junxion with Justyn");
+                holder.setTitleText("Billiards @ Mclarens");
                 holder.setDateText("March 30, 10:00PM");
                 holder.setDescriptionText("Come hangout with Justyn at Junxion.");
-                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.lightning));
+                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.billiards));
                 break;
             case 5:
-                holder.setTitleText("Lightning talks");
+                holder.setTitleText("Movie Night");
                 holder.setDateText("April 1, 7:00PM");
-                holder.setDescriptionText("Come hear what people have to say about lightning.");
-                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.lightning));
+                holder.setDescriptionText("Come watch movies and raise money for CHEO");
+                holder.setImgDrawable(context.getResources().getDrawable(R.drawable.movienight));
                 break;
         }
     }
